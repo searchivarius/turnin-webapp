ab3d7279125ceae9f3dc4a2de8624587c61a8c34
searchivarius <leo@boytsov.info>
2014-09-26 00:46:55 -0400

    ...

--- build.sh
+++ build.sh
@@ -5,11 +5,12 @@ set -u
 
 mkdir -p webapps/turnin/WEB-INF/classes
 scalac -version
-scalac -cp $(ls lib/*.jar webapps/turnin/WEB-INF/lib/*.jar | awk '{printf "%s:",$0}') \
+scalac -deprecation -cp $(ls lib/*.jar webapps/turnin/WEB-INF/lib/*.jar | awk '{printf "%s:",$0}') \
     -d webapps/turnin/WEB-INF/classes \
     src/*.scala
 
 echo >&2 "Copying libraries for current scala version..."
-SCALA=$(cd $(dirname $(which scala))/..; pwd)
-cp $SCALA/lib/scala-library.jar $PWD/webapps/turnin/WEB-INF/lib/
-cp $SCALA/lib/scala-compiler.jar $PWD/webapps/turnin/WEB-INF/lib/
+#SCALA=$(cd $(dirname $(which scala))/..; pwd)
+SCALA="/usr/share/java/"
+cp $SCALA/scala-library.jar $PWD/webapps/turnin/WEB-INF/lib/
+cp $SCALA/scala-compiler.jar $PWD/webapps/turnin/WEB-INF/lib/
